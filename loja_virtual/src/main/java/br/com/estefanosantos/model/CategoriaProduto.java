6df7ae454a868bc614217b5a0ef76730393aee09
Estéfano Santos Barros <estefanosantosbarros@gmail.com>
2024-05-10 11:46:01 -0400

    commit classe categoriaProduto que classifica os produtos em categorias

--- loja_virtual/src/main/java/br/com/estefanosantos/model/CategoriaProduto.java
+++ loja_virtual/src/main/java/br/com/estefanosantos/model/CategoriaProduto.java
@@ -18,16 +18,16 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable{
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
 	
-	private String marcaNome;
+	private String nomeCategoria;
 
 }
