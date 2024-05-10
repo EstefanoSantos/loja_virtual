d698e775c3a92b354876e02001554b347ce09acb
Estéfano Santos Barros <estefanosantosbarros@gmail.com>
2024-05-10 10:33:51 -0400

    configuração da conexão com o banco de dados postgresql e criação da classe MarcaProduto

---
+++ loja_virtual/src/main/java/br/com/estefanosantos/model/MarcaProduto.java
@@ -0,0 +1,29 @@
+package br.com.estefanosantos.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "seq_marca_produto")
+	private Long id;
+	
+	private String marcaNome;
+
+}
