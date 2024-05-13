65f57704320d76f786d317f4c252bfac7e7fb138
Estéfano Santos Barros <estefanosantosbarros@gmail.com>
2024-05-13 11:23:26 -0400

    Criação entidade de cumpom de desconto

---
+++ loja_virtual/src/main/java/br/com/estefanosantos/model/CupDesconto.java
@@ -0,0 +1,60 @@
+package br.com.estefanosantos.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "cupom_desconto")
+@SequenceGenerator(name = "seq_cup_desconto", sequenceName = "seq_cup_desconto", initialValue = 1, allocationSize = 1)
+public class CupDesconto implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cup_desconto")
+	private Long id;
+	
+	private String codigoDescricao;
+	
+	private BigDecimal valorRealDesc;
+	
+	private BigDecimal valorPorcentagemDesc;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataValidadeCupom;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CupDesconto other = (CupDesconto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
