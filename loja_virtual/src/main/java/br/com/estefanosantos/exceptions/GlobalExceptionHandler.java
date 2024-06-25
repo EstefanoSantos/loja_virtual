d770ed015dd7282cbafd231a7777684e217509d4
estef <estef@Estefano-PC>
2024-06-25 16:00:20 -0400

    Enviar email para exceções inesperadas de tempo de execução

--- loja_virtual/src/main/java/br/com/estefanosantos/exceptions/GlobalExceptionHandler.java
+++ loja_virtual/src/main/java/br/com/estefanosantos/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package br.com.estefanosantos.exceptions;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,38 +24,55 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.estefanosantos.dto.ObjectErroDto;
+import br.com.estefanosantos.service.impl.EmailService;
+import jakarta.mail.MessagingException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-	
-	@ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class})
+
+	@Autowired
+	EmailService emailService;
+
+	@ExceptionHandler({ Exception.class, RuntimeException.class, Throwable.class })
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatusCode status, WebRequest request) {
-		
+
 		ObjectErroDto erroDto = new ObjectErroDto();
-		
+
 		StringBuilder msg = new StringBuilder();
-		
+
 		if (ex instanceof MethodArgumentNotValidException) {
 			List<ObjectError> list = ((MethodArgumentNotValidException) ex).getBindingResult().getAllErrors();
-			
+
 			for (ObjectError objError : list) {
-				msg.append(objError.getDefaultMessage()).append("\n");			
+				msg.append(objError.getDefaultMessage()).append("\n");
 			}
-		} else if (ex instanceof HttpMessageNotReadableException) {			
-			msg.append("Corpo da requisição nulo ou mal formatado.");	
+		} else if (ex instanceof HttpMessageNotReadableException) {
+			msg.append("Corpo da requisição nulo ou mal formatado.");
 		} else {
 			msg.append(ex.getMessage());
 		}
-		
+
 		erroDto.setErro(msg.toString());
 		erroDto.setStatus(status.toString());
-		
+
+		try {
+
+			emailService.sendEmailHtml("estefano_mx@hotmail.com", "Erro inesperado na Loja Virtual",
+					ExceptionUtils.getStackTrace(ex));
+
+		} catch (UnsupportedEncodingException e) {
+
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+
 		return new ResponseEntity<>(erroDto, HttpStatus.INTERNAL_SERVER_ERROR);
-		
-	} 
-	
+
+	}
+
 	@ExceptionHandler({DataIntegrityViolationException.class, ConstraintViolationException.class, SQLException.class})
 	protected ResponseEntity<Object> handleDataIntegrityException(Exception ex) {
 		
@@ -72,17 +92,28 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		erroDto.setErro(msg.toString());
 		erroDto.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.toString());
+		
+	try {
 			
-		return new ResponseEntity<>(erroDto, HttpStatus.INTERNAL_SERVER_ERROR);
+			emailService.sendEmailHtml("estefano_mx@hotmail.com", "Erro inesperado na Loja Virtual"
+					, ExceptionUtils.getStackTrace(ex));
+			
+		} catch (UnsupportedEncodingException e) {
+			
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+					return new ResponseEntity<>(erroDto, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
+
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<Object> handleCustomException(CustomException ex) {
 		ObjectErroDto erroDto = new ObjectErroDto();
-		
+
 		erroDto.setErro(ex.getMessage());
 		erroDto.setStatus(HttpStatus.OK.toString());
-		
+
 		return new ResponseEntity<>(erroDto, HttpStatus.OK);
 	}
 
@@ -95,6 +126,5 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<String> handleJwtException(JwtException ex) {
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Cannot generate token.");
 	}
-		
+
 }
- 
